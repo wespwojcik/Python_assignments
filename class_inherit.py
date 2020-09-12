bc8eaf170992542eaa3dc1a0c7e124baa7139727
wespwojcik <67170488+wespwojcik@users.noreply.github.com>
2020-09-12 14:36:52 -0700

    Create class_inherit.py

---
+++ class_inherit.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+class User:
+    #Define the attributes of the class
+    name = 'no name provided'
+    email = ' '
+    password = 'hellohello'
+    account = 0
+
+
+
+class Employee(User)
+    hourly_pay = 47.00
+    department = 'health care'
+
+
+class Customer(User):
+    mailing_address = ' '
+    mailing_list = True
