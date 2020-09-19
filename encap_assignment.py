83bf1fa3642ed25555ae079b0449c0a716b99ed3
wespwojcik <67170488+wespwojcik@users.noreply.github.com>
2020-09-19 16:08:48 -0700

    Update encap_assignment.py

--- encap_assignment.py
+++ encap_assignment.py
@@ -17,3 +17,20 @@ obj = Protected()
 obj.getprivate()
 obj.setprivate(45)
 obj.getprivate()
+
+
+
+class pvate:
+    def __init__(self):
+        self.__privateVar = 13
+
+    def getPrivate(self):
+        print(self.__privateVar)
+
+    def setPrivate(self, private):
+        self.__privateVar = private
+
+obj = pvate()
+obj.getPrivate()
+obj.setPrivate()
+obj.getPrivate()
