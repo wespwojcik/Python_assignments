e0550f9c43748696c1446ae58003915ec7c01f83
wespwojcik <67170488+wespwojcik@users.noreply.github.com>
2020-09-24 13:36:47 -0700

    Create main_script2.py

---
+++ main_script2.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+from datetime import datetime , timedelta
+from tzlocal import get_localzone
+import time
+from tkinter import *
+from tkinter import filedialog as fd
+
+
+
+destination = '/Users/weswo/OneDrive/Desktop/python_assignments/shutil_assignmentpt2/reciever/'
+
+
+
+def fun():
+    Open = fd.askdirectory()
+    files = os.listdir(Open)
+    g = datetime.now()
+    for i in files:
+        NewOpen = Open + '/' + i
+        f =  os.path.getmtime(NewOpen)
+        local_time = datetime.fromtimestamp(f)
+        var2 = g - timedelta(1)
+        print('24 hoursago')
+        print( var2)
+        
+        if (var2 - local_time) < timedelta(1):
+            shutil.move(NewOpen,destination)
+
+            
+win = Tk()
+win.minsize(600,250)
+win.maxsize(600,250)
+b1 = Button(win,text="Open",command = fun)
+b1.grid(row=0,column=1)
+
+
+
+        
+
+
+
+
+
